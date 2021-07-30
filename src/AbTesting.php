79dffb9220db8a68b9e2e789452fda30bf6c15fd
glema <gleman17@gmail.com>
2021-07-30 11:03:29 -0600

    Expand allow_ab_exp to allow values per environment.

--- src/AbTesting.php
+++ src/AbTesting.php
@@ -129,7 +129,9 @@ class AbTesting
     {
         $this->pageView();
 
-        if (config('ab-testing.allow_ab_exp')[env('APP_ENV_STATUS')]??false){
+        $use_ab_in_this_environment = config('ab-testing.allow_ab_exp')
+                    [env('APP_ENV_STATUS')] ?? false;
+        if ($use_ab_in_this_environment){
             $experiment_name = Request::input('ab_exp', null);
             if ($experiment_name !== null) {
                 return $experiment_name === $name;
